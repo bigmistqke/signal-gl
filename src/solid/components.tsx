b3297c8d85051d2bdc9e5d5fdb20af8bb4a057a7
bigmistqke <vandijckv@gmail.com>
2023-11-28 18:27:43 +0100

    clear start renderloop

--- src/solid/components.tsx
+++ src/solid/components.tsx
@@ -80,6 +80,11 @@ export const GL = (
           const gl = _canvas?.getContext('webgl2')
           if (!_canvas || !gl) return
 
+          gl.clearColor(0.0, 0.0, 0.0, 1.0) // Clear to black, fully opaque
+          gl.clearDepth(1.0) // Clear everything
+          gl.enable(gl.DEPTH_TEST) // Enable depth testing
+          gl.depthFunc(gl.LEQUAL) // Near things obscure far things
+
           gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
 
           const childs = memoChildren()
